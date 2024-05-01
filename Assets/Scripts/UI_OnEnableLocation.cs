65f36ffb8c54ae66a4e18ba316d723b5726c7beb
Jordi Alfonso Poza <jordialfonsop@gmail.com>
2024-05-01 15:55:26 +0200

    UI Location Correction

---
+++ Assets/Scripts/UI_OnEnableLocation.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIOnEnableLocation : MonoBehaviour
+{
+    [SerializeField] private GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        float xpos = 0;
+        float ypos = 0;
+        float zpos = 0;
+
+        float distance = 0.25f;
+
+        if (target.transform.forward.x > 0)
+        {
+            xpos = (float)(target.transform.position.x + distance);
+        }else if (target.transform.forward.x < 0)
+        {
+            xpos = (float)(target.transform.position.x - distance);
+        }
+        else
+        {
+            xpos = (float)target.transform.position.x;
+        }
+
+        if (target.transform.forward.y > 0)
+        {
+            ypos = (float)(target.transform.position.y + distance);
+        }
+        else if (target.transform.forward.y < 0)
+        {
+            ypos = (float)(target.transform.position.y - distance);
+        }
+        else
+        {
+            ypos = (float)target.transform.position.y;
+        }
+
+        if (target.transform.forward.z > 0)
+        {
+            zpos = (float)(target.transform.position.z + distance);
+        }
+        else if (target.transform.forward.z < 0)
+        {
+            zpos = (float)(target.transform.position.z - distance);
+        }
+        else
+        {
+            zpos = (float)target.transform.position.z;
+            
+        }
+
+        Debug.Log("zpos: " + target.transform.position.z);
+
+        this.transform.position = new Vector3(xpos, ypos, zpos);
+        transform.rotation = Quaternion.LookRotation(transform.position - target.transform.position);
+    }
+}
